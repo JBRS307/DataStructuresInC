e5db70ef51f92ab2a81041f7ae3cd0e974cdbc8d
JBRS307 <kuba.rekas.307@gmail.com>
2024-07-04 11:41:40 +0200

    operations to push, peek and pop done, added function to initialize queue dynamically

--- queue/queue.c
+++ queue/queue.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 #include "queue.h"
 
@@ -11,10 +12,85 @@ Queue queue_init() {
     return q;
 }
 
+Queue* queue_init_dynamic() {
+    Queue* q = (Queue*)malloc(sizeof(Queue));
+
+    q->head = NULL;
+    q->tail = NULL;
+    q->size = 0;
+    return q;
+}
+
 bool queue_is_empty(const Queue* q) {
     return q->size == 0;
 }
 
 size_t queue_size(const Queue* q) {
     return q->size;
+}
+
+QueueStatusCode queue_peek(const Queue* q, void** data, size_t* nmeb) {
+    if (data == NULL) {
+        return NULL_DATA_ERROR;
+    }
+    if (queue_is_empty(q)) {
+        return QUEUE_EMPTY_ERROR;
+    }
+
+    *data = q->head->data;
+    if (nmeb != NULL) {
+        *nmeb = q->head->nmeb;
+    }
+
+    return SUCCESS;
+}
+
+QueueStatusCode queue_push(Queue* q, void* data, size_t nmeb) {
+    if (data == NULL) {
+        return NULL_DATA_ERROR;
+    }
+
+    QueueNode* node = (QueueNode*)malloc(sizeof(QueueNode));
+    node->data = malloc(nmeb);
+    if (node->data == NULL) {
+        return ALLOCATION_ERROR;
+    }
+
+    memcpy(node->data, data, nmeb);
+    node->nmeb = nmeb;
+
+    node->prev = q->tail;
+    node->next = NULL;
+    q->tail = node;
+    if (queue_is_empty(q)) {
+        q->head = node;
+    }
+    q->size++;
+
+    return SUCCESS;
+}
+
+QueueStatusCode queue_pop(Queue* q, void** data, size_t* nmeb) {
+    if (data == NULL) {
+        return NULL_DATA_ERROR;
+    }
+    if (queue_is_empty(q)) {
+        return QUEUE_EMPTY_ERROR;
+    }
+
+    QueueNode* removed = q->head;
+    q->head = removed->next;
+    if (q->size == 1) {
+        q->tail = NULL;
+    }
+    q->size--;
+
+    *data = removed->data;
+    if (nmeb != NULL) {
+        *nmeb = removed->nmeb;
+    }
+
+
+    free(removed);
+    return SUCCESS;
 }
--- queue/queue.h
+++ queue/queue.h
@@ -4,10 +4,13 @@
 
 typedef enum queue_status_code {
     SUCCESS,
+    QUEUE_EMPTY_ERROR,
+    NULL_DATA_ERROR,
+    ALLOCATION_ERROR,
 } QueueStatusCode;
 
 typedef struct queue_node {
-    void* elem;
+    void* data;
     struct queue_node* next;
     struct queue_node* prev;
     size_t nmeb;
@@ -22,9 +25,21 @@ typedef struct queue {
 // initializes empty queue
 Queue queue_init();
 
+// dynamically initializes empty queue
+Queue* queue_init_dynamic();
+
 // checks if queue is empty
 bool queue_is_empty(const Queue* q);
 
 // returns size of the queue
 size_t queue_size(const Queue* q);
 
+// saves first element in data, if nmeb given saves its size 
+QueueStatusCode queue_peek(const Queue* q, void** data, size_t* nmeb);
+
+// pushes data of size nmeb into queue. It copies data from data into queue
+QueueStatusCode queue_push(Queue* q, void* data, size_t nmeb);
+
+// Removes element from queue, saves data in data.
+// If nmeb is not NULL then saves removed element's size
+QueueStatusCode queue_pop(Queue* q, void** data, size_t* nmeb);
