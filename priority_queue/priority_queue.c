2a773546615baedfe23788303802682f195ec19f
JBRS307 <kuba.rekas.307@gmail.com>
2024-07-04 14:10:11 +0200

    removed type definition for comparator function and renamed property to compar

--- priority_queue/priority_queue.c
+++ priority_queue/priority_queue.c
@@ -28,65 +28,65 @@ static void swap(void* a, void* b, size_t nmeb) {
     free(temp);
 }
 
-static void heapify(void* array, size_t i, size_t size, size_t nmeb, comparator f) {
+static void heapify(void* array, size_t i, size_t size, size_t nmeb, int (*compar)(const void*, const void*)) {
     size_t l, r, min_ind;
     l = left(i);
     r = right(i);
     min_ind = i;
 
-    if (l < size && f(array+offset(l, nmeb), array+offset(min_ind, nmeb)) < 0) {
+    if (l < size && compar(array+offset(l, nmeb), array+offset(min_ind, nmeb)) < 0) {
         min_ind = l;
     }
-    if (r < size && f(array+offset(r, nmeb), array+offset(min_ind, nmeb)) < 0) {
+    if (r < size && compar(array+offset(r, nmeb), array+offset(min_ind, nmeb)) < 0) {
         min_ind = r;
     }
 
     if (min_ind != i) {
         swap(array+offset(i, nmeb), array+offset(min_ind, nmeb), nmeb);
-        heapify(array, min_ind, size, nmeb, f);
+        heapify(array, min_ind, size, nmeb, compar);
     }
 }
 
-static void move_up(void* array, size_t i, size_t nmeb, comparator f) {
+static void move_up(void* array, size_t i, size_t nmeb, int (*compar)(const void*, const void*)) {
     if (i == 0) {
         return;
     }
 
     size_t p = parent(i);
 
-    if (f(array+offset(i, nmeb), array+offset(p, nmeb)) < 0) {
+    if (compar(array+offset(i, nmeb), array+offset(p, nmeb)) < 0) {
         swap(array+offset(i, nmeb), array+offset(p, nmeb), nmeb);
-        move_up(array, p, nmeb, f);
+        move_up(array, p, nmeb, compar);
     }
 }
 
-static void build_heap(void* array, size_t size, size_t nmeb, comparator f) {
+static void build_heap(void* array, size_t size, size_t nmeb, int (*compar)(const void*, const void*)) {
     for (size_t i = parent(size-1); i >= 0; i--) {
-        heapify(array, i, size, nmeb, f);
+        heapify(array, i, size, nmeb, compar);
     }
 }
 
 // end of static functions
 
-PriorityQueue pq_init(void* array, comparator f, size_t nmeb, size_t size) {
+PriorityQueue pq_init(void* array, int (*compar)(const void*, const void*), size_t nmeb, size_t size) {
     PriorityQueue pq;
     if (array != NULL) {
         pq.array = malloc(size*nmeb);
         memcpy(pq.array, array, size*nmeb);
         pq.size = size;
         pq.capacity = size;
-        pq.nmeb = nmeb;
-        build_heap(pq.array, size, nmeb, f);
+        build_heap(pq.array, size, nmeb, compar);
     } else {
         pq.array = malloc(nmeb);
         pq.size = 0;
         pq.capacity = 1;
-        pq.nmeb = nmeb;
     }
+    pq.nmeb = nmeb;
+    pq.compar = compar;
     return pq;
 }
 
-PriorityQueue* pq_init_dynamic(void* array, comparator f, size_t nmeb, size_t size) {
+PriorityQueue* pq_init_dynamic(void* array, int (*compar)(const void*, const void*), size_t nmeb, size_t size) {
     PriorityQueue* pq = (PriorityQueue*)malloc(sizeof(PriorityQueue));
     if (pq == NULL) {
         return NULL;
@@ -97,14 +97,14 @@ PriorityQueue* pq_init_dynamic(void* array, comparator f, size_t nmeb, size_t si
         memcpy(pq->array, array, size*nmeb);
         pq->size = size;
         pq->capacity = size;
-        pq->nmeb = nmeb;
-        build_heap(pq->array, size, nmeb, f);
+        build_heap(pq->array, size, nmeb, compar);
     } else {
         pq->array = malloc(nmeb);
         pq->size = 0;
         pq->capacity = 1;
-        pq->nmeb = nmeb;
     }
+    pq->nmeb = nmeb;
+    pq->compar = compar;
     return pq;
 }
 
@@ -133,7 +133,7 @@ PriorityQueueStatusCode pq_put(PriorityQueue* pq, const void* data) {
     size_t index = pq->size;
     memcpy(pq->array+offset(index, pq->nmeb), data, pq->nmeb);
 
-    move_up(pq->array, index, pq->nmeb, pq->f);
+    move_up(pq->array, index, pq->nmeb, pq->compar);
     pq->size++;
     return SUCCESS;
 }
@@ -152,7 +152,7 @@ PriorityQueueStatusCode pq_pop(PriorityQueue* pq, void* data) {
     swap(pq->array, pq->array+offset(pq->size - 1, pq->nmeb), pq->nmeb);
     pq->size--;
 
-    heapify(pq->array, 0, pq->size, pq->nmeb, pq->f);
+    heapify(pq->array, 0, pq->size, pq->nmeb, pq->compar);
 
     return SUCCESS;
 }
--- priority_queue/priority_queue.h
+++ priority_queue/priority_queue.h
@@ -9,22 +9,20 @@ typedef enum priority_queue_status_code {
     ALLOCATION_ERROR,
 } PriorityQueueStatusCode;
 
-typedef int (*comparator)(const void*, const void*);
-
 typedef struct priority_queue {
     void* array;
     size_t size;
     size_t capacity;
     size_t nmeb;
-    comparator f;
+    int (*compar)(const void*, const void*);
 } PriorityQueue;
 
 // Initializes PriorityQueue with array of size size with each
 // element of size nmeb. If array is NULL then size is ignored
 // Array is copied into priority queue
-PriorityQueue pq_init(void* array, comparator f, size_t nmeb, size_t size);
+PriorityQueue pq_init(void* array, int (*compar)(const void*, const void*), size_t nmeb, size_t size);
 
-PriorityQueue* pq_init_dynamic(void* array, comparator f, size_t nmeb, size_t size);
+PriorityQueue* pq_init_dynamic(void* array, int (*compar)(const void*, const void*), size_t nmeb, size_t size);
 
 // Peeks the top element from the priority queue, copies it into data
 PriorityQueueStatusCode pq_peek(const PriorityQueue* pq, void* data);
